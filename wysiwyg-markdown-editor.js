9fe478c29d049fbb069bb8aea3c671aaa8c04b8e
Araz Gholami <heshzad@gmail.com>
2025-06-07 08:35:44 +0330

    Better mobile responibilty

--- wysiwyg-markdown-editor.js
+++ wysiwyg-markdown-editor.js
@@ -1,6 +1,6 @@
 /**
  * WYSIWYG Markdown Editor
- * v=1.1
+ * v=1.2
  * A lightweight, real-time markdown editor with live rendering and LTR-RTL support
  * Usage: MarkdownEditor.init('your-div-id');
  * Author: Araz Gholami @arazgholami
