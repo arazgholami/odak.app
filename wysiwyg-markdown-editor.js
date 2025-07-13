a94f50068e0b61531d7438d93a1c503aa4c0bc7e
Araz Gholami <heshzad@gmail.com>
2025-07-13 10:10:28 +0330

    Update Version

--- wysiwyg-markdown-editor.js
+++ wysiwyg-markdown-editor.js
@@ -1,6 +1,6 @@
 /**
  * WYSIWYG Markdown Editor
- * v=2.0
+ * v=2.1
  * A lightweight, real-time markdown editor with live rendering and LTR-RTL support
  * Usage: MarkdownEditor.init('your-div-id');
  * Author: Araz Gholami @arazgholami
